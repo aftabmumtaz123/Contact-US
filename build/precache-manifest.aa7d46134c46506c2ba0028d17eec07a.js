a54221cfe31c180c6ab753853d323b7cb0ee90c5
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2024-07-19 17:25:22 +0500

    Add files via upload

---
+++ build/precache-manifest.aa7d46134c46506c2ba0028d17eec07a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628d8e4b50ca16fc56aca1a748993428",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcb63f4980329880b639",
+    "url": "/static/css/main.1624d5f1.chunk.css"
+  },
+  {
+    "revision": "c0ee305eb3145ecf5a2b",
+    "url": "/static/js/2.07b41d29.chunk.js"
+  },
+  {
+    "revision": "fcb63f4980329880b639",
+    "url": "/static/js/main.a09fbf40.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
